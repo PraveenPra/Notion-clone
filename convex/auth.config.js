2b8bacc68dd187fa7a8647e1b58e5bf196a159bb
PraveenPra <prvp6501@gmail.com>
2023-12-26 22:47:34 +0530

    Setup convex and clerk

---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+// https://artistic-camel-77.clerk.accounts.dev
+
+export default {
+    providers: [
+      {
+        domain: "https://artistic-camel-77.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
