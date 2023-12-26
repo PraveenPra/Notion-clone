e32522a5204d107151de366cafc4e47c9e45b8b1
PraveenPra <prvp6501@gmail.com>
2023-12-26 23:15:04 +0530

    Added spinner component

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -5,8 +5,10 @@ import { cn } from "@/lib/utils";
 import Logo from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
 import { useConvexAuth } from "convex/react";
-import { SignInButton } from "@clerk/clerk-react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 const Navbar = () => {
     const { isAuthenticated,isLoading} = useConvexAuth();
@@ -20,7 +22,7 @@ const Navbar = () => {
 
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
                 {isLoading && (
-                    <p>Loading...</p>
+                   <Spinner/>
                 )}
 
                 {!isAuthenticated && !isLoading && (
@@ -37,6 +39,15 @@ const Navbar = () => {
                         </Button>
                     </SignInButton>
                     </>
+                )}
+
+                {isAuthenticated && !isLoading && (
+                    <>
+                    <Button variant='ghost' size='sm'>
+                        <Link href='/documents'>
+                        Enter </Link></Button>
+                        <UserButton afterSignOutUrl="/"/>
+                        </>
                 )}
                 <ModeToggle/>
             </div>
