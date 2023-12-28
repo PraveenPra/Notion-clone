af154f032f89764acbb8f405a742e8959063a14d
PraveenPra <prvp6501@gmail.com>
2023-12-28 21:40:36 +0530

    User dashboard page design

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,6 +1,39 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
+
 const DocumentsPage = () => {
-    return ( <div>
-        This is a protected page
+
+    const {user} = useUser();
+console.log(user)
+    return ( <div className="h-full flex flex-col items-center justify-center space-y-4">
+       <Image
+       src='/empty.png'
+       alt="empty"
+       width={300}
+       height={300}
+       className="dark:hidden"
+       />
+
+<Image
+       src='/empty-dark.png'
+       alt="empty"
+       width={300}
+       height={300}
+       className="hidden dark:block"
+       />
+
+<h2 className="text-lg font-medium">
+    Welcome to {user?.firstName}&apos;s Jotion
+</h2>
+
+<Button>
+    <PlusCircle className="h-4 w-4 mr-2"/>
+    Create a note
+</Button>
     </div> );
 }
  
