39ab428ad1201d5d605d2c1bb133e4a28d530c26
PraveenPra <prvp6501@gmail.com>
2024-01-03 00:10:04 +0530

    Changed notion to jotion for marketing page

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -11,48 +11,49 @@ import { Spinner } from "@/components/spinner";
 import Link from "next/link";
 
 const Navbar = () => {
-    const { isAuthenticated,isLoading} = useConvexAuth();
+    const { isAuthenticated, isLoading } = useConvexAuth();
     const scrolled = useScrollTop();
 
-    return ( 
+    return (
         <div className={cn(
             "z-50 bg-background dark:bg-[#1f1f1f] fixed top-0 flex items-center w-full p-6", scrolled && "border-b shadow-sm"
         )}>
-            <Logo/>
+            <Logo />
 
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
                 {isLoading && (
-                   <Spinner/>
+                    <Spinner />
                 )}
 
                 {!isAuthenticated && !isLoading && (
                     <>
-                    <SignInButton mode="modal">
-                        <Button variant='ghost' size='sm'>
-                            Login
-                        </Button>
-                    </SignInButton>
+                        <SignInButton mode="modal">
+                            <Button variant='ghost' size='sm'>
+                                Login
+                            </Button>
+                        </SignInButton>
 
-                    <SignInButton mode="modal">
-                        <Button size='sm'>
-                            Get Jotion free
-                        </Button>
-                    </SignInButton>
+                        <SignInButton mode="modal">
+                            <Button size='sm'>
+                                Get Jotion free
+                            </Button>
+                        </SignInButton>
                     </>
                 )}
 
                 {isAuthenticated && !isLoading && (
                     <>
-                    <Button variant='ghost' size='sm'>
-                        <Link href='/documents'>
-                        Enter </Link></Button>
-                        <UserButton afterSignOutUrl="/"/>
-                        </>
+                        <Button variant='ghost' size='sm'>
+                            <Link href='/documents'>
+                                Enter </Link>
+                        </Button>
+                        <UserButton afterSignOutUrl="/" />
+                    </>
                 )}
-                <ModeToggle/>
+                <ModeToggle />
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Navbar;
