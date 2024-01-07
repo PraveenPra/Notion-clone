3c923f6b9eefae8f3bd0dcc3b4f7207429d4ab5b
PraveenPra <prvp6501@gmail.com>
2024-01-07 22:36:17 +0530

    Archive functionality

--- convex/documents.ts
+++ convex/documents.ts
@@ -17,7 +17,52 @@ import {Doc, Id} from "./_generated/dataModel"
 //         return documents;
 //     }
 // })
+export const archive = mutation({
+    args: {id: v.id("documents")},
+    handler: async (ctx,args)=>{
+        if(!identity){
+            throw new Error("Not authenticated")
+        }
 
+        const userId = identity.subject;
+
+        const existingDocument = await ctx.db.get(args.id);
+
+        if(!existingDocument){
+            throw new Error("Not found");
+        }
+
+        if(existingDocument.userId !== userId){
+            throw new Error("Unauthorised")
+        }
+
+        const recursiveArchive = async (documentId: Id<"documents">)=>{
+            const children = await ctx.db
+            .query("documents")
+            .withIndex("by_user_parent",(q)=>(
+                q.eq("userId",userId)
+                .eq("parentDocument",documentId)
+            ))
+            .collect();
+
+            // we cant use foreach here , as it cant have await inside it 
+            for(const child of children){
+                await ctx.db.patch(child._id,{
+                    isArchived:true,
+                })
+
+                await recursiveArchive(child._id);
+            }
+        }
+        const document = await ctx.db.patch(args.id,{
+            isArchived:true
+        })
+
+        recursiveArchive(args.id);
+        
+        return document;
+    }
+})
 export const getSidebar = query({
     args:{
         parentDocument: v.optional(v.id("documents"))
