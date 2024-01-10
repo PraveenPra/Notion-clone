15a54217a1a2c9a1819e73b3aaee807510d77e8d
PraveenPra <prvp6501@gmail.com>
2024-01-11 01:07:02 +0530

    Delete option working

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { ConfirmModal } from "@/components/modals/confirm-modal";
 import { Spinner } from "@/components/spinner";
 import { Input } from "@/components/ui/input";
 import { api } from "@/convex/_generated/api";
@@ -93,15 +94,17 @@ return(
 <div className="flex items-center">
 <div  
 role="button"
-    onClick={()=>onRestore(e,document._id)}
+    onClick={(e)=>onRestore(e,document._id)}
     className="p-2 rounded-sm hover:bg-neutral-200">
 <Undo className="h-4 w-4 text-muted-foreground"/>
 </div>
 
+<ConfirmModal onConfirm={()=>onRemove(document._id)}>
 <div className="rounded-sm p-2 hover:bg-neutral-200"
 role="button">
     <Trash className="h-4 w4 text-muted-foreground"/>
 </div>
+</ConfirmModal>
 </div>
     </div>
 ))}
