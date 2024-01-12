fd6c7a416957711d60550cb5162e8b7a8fb3f98f
PraveenPra <prvp6501@gmail.com>
2024-01-12 23:46:56 +0530

    Getsearch api

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -5,3 +5,13 @@ import { File } from "lucide-react"
 import { useQuery } from "convex/react"
 import { useRouter } from "next/navigation"
 import { useUser } from "@clerk/clerk-react"
+
+import { CommandDialog } from "@/components/ui/command"
+import { useSearch } from "@/hooks/use-search"
+
+export const SearchCommand = () => {
+    const {user} =useUser()
+    const router = useRouter()
+    return (  );
+}
+ 
