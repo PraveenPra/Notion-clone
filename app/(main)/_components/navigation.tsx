efe4d520fb8fc6f4f90e7de26ec5de192be69577
PraveenPra <prvp6501@gmail.com>
2024-01-16 23:21:25 +0530

    Navbar inside main screen

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from "@/lib/utils";
 import { ChevronsLeft, Menu, MenuIcon, Plus, PlusCircle, Search, Settings, Trash } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import UserItem from "./user-item";
@@ -15,8 +15,12 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import { TrashBox } from "./trash-box";
 import { useSearch } from "@/hooks/use-search";
 import { useSettings } from "@/hooks/use-settings";
+import { Navbar } from "./navbar";
 
 const Navigation = () => {
+
+    const params = useParams()
+
     const pathname = usePathname(); //to use to keep sidebar clone unless user clicks on a document
     const isMobile = useMediaQuery("(max-width: 768px)");//we use this for dragging sidebar for collapsing. its complex to define breakpoints without this hook/trick
 
@@ -189,9 +193,18 @@ const Navigation = () => {
                 isResetting && "transition-all ease-in-out duration-300",
                 isMobile && "left-0 w-full"
             )}>
-            <nav className="bg-transparent px-3 py-2 w-full">
+                {/* !! is used to turn a string into boolean */}
+                {!!params.documentId ? (
+                    <Navbar
+                    isCollapsed = {isCollapsed}
+                    onResetWidth = {resetWidth}/>
+                ):(
+ <nav className="bg-transparent px-3 py-2 w-full">
                 {isCollapsed && <MenuIcon onClick={resetWidth} className="h-6 w-6 text-muted-foreground" />}
             </nav>
+                )}
+               
+           
         </div>
     </>
     );
