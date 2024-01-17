76d8b51ead056f5f690acc91fe5bdac12bc4110b
PraveenPra <prvp6501@gmail.com>
2024-01-17 10:50:40 +0530

    Added skeleton for the title

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -22,7 +22,13 @@ export const Navbar = (
         //ID<""> is used because typescrpt docid expects id type but params give string type, 
 
         if(document === undefined){
-            return <p>loading...</p>
+            return (
+                <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
+                <Title.Skeleton />
+                <div className="flex items-center gap-x-2">
+                  {/* <Menu.Skeleton /> */}
+                </div>
+            </nav>)
         }
 
         if(document === null){
