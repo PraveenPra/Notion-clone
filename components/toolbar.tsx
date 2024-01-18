183c6f3a527f2efda630d517d8ee69a2f2ce6187
PraveenPra <prvp6501@gmail.com>
2024-01-18 21:05:59 +0530

    Temp commit

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -4,7 +4,10 @@ import { Doc } from "@/convex/_generated/dataModel";
 import { IconPicker } from "./icon-picker";
 import { Button } from "./ui/button";
 import { ImageIcon, Smile, X } from "lucide-react";
-
+import { ElementRef, useRef, useState } from "react";
+import { useMutation } from "convex/react";
+import TextareaAutosize from 'react-textarea-autosize';
+import { api } from "@/convex/_generated/api";
 
 interface ToolbarProps {
     initialData: Doc<"documents">;
@@ -15,6 +18,44 @@ export const Toolbar = ({
     initialData,
     preview
 }: ToolbarProps) => {
+    const inputRef = useRef<ElementRef<"textarea">>(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [value, setValue] = useState(initialData.title);
+
+  const update = useMutation(api.documents.update);
+
+  
+  const enableInput = () => {
+    if (preview) return;//guest shouldnt be able to do this so
+
+    setIsEditing(true);
+    setTimeout(() => {
+      setValue(initialData.title);
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  
+  const disableInput = () => setIsEditing(false);
+
+  const onInput = (value: string) => {
+    setValue(value);
+    update({
+      id: initialData._id,
+      title: value || "Untitled"
+    });
+  };
+
+  
+  const onKeyDown = (
+    event: React.KeyboardEvent<HTMLTextAreaElement>
+  ) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      disableInput();
+    }
+  };
+
     return (
         <div className="pl-[54px] group relative">
             {!!initialData.icon && !preview && (
@@ -66,6 +107,23 @@ export const Toolbar = ({
           </Button>
         )}
         </div>
+        {isEditing && !preview ? (
+        <TextareaAutosize
+          ref={inputRef}
+          onBlur={disableInput}
+          onKeyDown={onKeyDown}
+          value={value}
+          onChange={(e) => onInput(e.target.value)}
+          className="text-5xl bg-transparent font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF] resize-none"
+        />
+      ) : (
+        <div
+          onClick={enableInput}
+          className="pb-[11.5px] text-5xl font-bold break-words outline-none text-[#3F3F3F] dark:text-[#CFCFCF]"
+        >
+          {initialData.title}
+        </div>
+      )}
         </div>
     )
 }
